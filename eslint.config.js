77cb79cdd5825f1312b357ed739e57261e815c3c
ikezaki <ikezaki@social-db.co.jp>
2025-03-29 01:15:16 +0900

    Update ESLint config to include custom spacing rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,18 @@ import tseslint from "typescript-eslint";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+    "keyword-spacing": ["error", { "before": true, "after": true }],
+    "func-call-spacing": ["error", "never"],
+    "space-in-parens": ["error", "never"],
+    "space-infix-ops": ["error", { "int32Hint": false }],
+    "block-spacing": ["error", "always"],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    },
+  },
   tseslint.configs.recommended,
 ]);
