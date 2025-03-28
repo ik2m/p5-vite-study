d19dded411374f8114cf482e3ae6928f8a2d085d
ikezaki <ikezaki@social-db.co.jp>
2025-03-29 01:18:43 +0900

    fmt

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
-import './style.css'
-import p5 from 'p5'
-
+import "./style.css";
+import p5 from "p5";
 
 const sketch = (p: p5) => {
-
   const WHITE = p.color(255);
-  const createBaseRect = () =>{
+  const createBaseRect = () => {
     p.translate(p.width / 2, p.height / 2);
     p.rotate(45);
     p.noStroke();
     p.fill(WHITE);
     p.rect(0, 0, 100, 100, 10);
-  }
+  };
 
   p.setup = () => {
     const canvas = p.createCanvas(400, 400);
-    canvas.parent('app');
+    canvas.parent("app");
 
     p.rectMode(p.CENTER);
   };
