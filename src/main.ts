b5ef521ed2c1dada4a4dde2dd3c21d26f0a76be1
ik2m <39111330+ik2m@users.noreply.github.com>
2025-03-28 14:04:21 +0900

    add rectangle drawing function to main sketch

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,27 @@ import p5 from 'p5'
 
 
 const sketch = (p: p5) => {
+
+  const WHITE = p.color(255);
+  const createBaseRect = () =>{
+    p.translate(p.width / 2, p.height / 2);
+    p.rotate(45);
+    p.noStroke();
+    p.fill(WHITE);
+    p.rect(0, 0, 100, 100, 10);
+  }
+
   p.setup = () => {
     const canvas = p.createCanvas(400, 400);
     canvas.parent('app');
+
+    p.rectMode(p.CENTER);
   };
 
   p.draw = () => {
     p.background(220);
-    p.ellipse(p.width / 2, p.height / 2, 50, 50);
+
+    createBaseRect();
   };
 };
 
